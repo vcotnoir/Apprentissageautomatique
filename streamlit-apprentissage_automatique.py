d93509195f6e999a8ea6c365d2ceb2975e49b8d9
Vincent Cotnoir <120339475+vcotnoir@users.noreply.github.com>
2023-11-25 15:58:43 -0500

    Add files via upload

---
+++ streamlit-apprentissage_automatique.py
@@ -0,0 +1,17 @@
+import seaborn as sns
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title('Apprentissage automatique - prédictions UFC')
+
+st.text('Notre projet consiste à faire les meilleures prédictions possibles sur des données de UFC')
+
+st.header('Voici les données brutes:')
+#importation des données
+url = 'https://raw.githubusercontent.com/vcotnoir/Apprentissageautomatique/main/ufc-master-final.csv'
+df = pd.read_csv(url)
+
+st.dataframe(df)
+
