50248803be6037d2235618c34668ba168bef690c
Vincent Cotnoir <cotnoir.vincent@gmail.com>
2023-11-25 16:44:38 -0500

    Update streamlit-apprentissage_automatique.py

--- streamlit-apprentissage_automatique.py
+++ streamlit-apprentissage_automatique.py
@@ -3,12 +3,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
-st.title('Apprentissage automatique - prédictions UFC')
-
-st.text('Notre projet consiste à faire les meilleures prédictions possibles sur des données de UFC')
-
-st.header('Voici les données brutes:')
 #importation des données
 url = 'https://raw.githubusercontent.com/vcotnoir/Apprentissageautomatique/main/ufc-master-final.csv'
 df = pd.read_csv(url)
@@ -16,14 +10,23 @@ df = pd.read_csv(url)
 url_preprocessed = 'https://raw.githubusercontent.com/vcotnoir/Apprentissageautomatique/main/UFC_dataprocessed.csv'
 df_preprocessed = pd.read_csv(url_preprocessed)
 
-st.text('Seulement 69 colonnes ont étés gardés sur les 119 présentent originalement')
-st.text('Les changements suivants ont étés faits:') 
+shape_df=df.shape
+shape_dr_processed=df_preprocessed.shape
+
+st.title('Apprentissage automatique - prédictions UFC')
+
+intro_text='''Faire les meilleures prédictions possibles  
+            sur les combats UFC '''
+
+st.markdown(intro_text)
 
 on=st.toggle('appuyez pour voir les données traitées')
 
 if on:
-    st.header('Données prétraitées')
+    st.header('Données traitées')
     st.dataframe(df_preprocessed)
+
+
 else:
     st.header('Données brutes')
     st.dataframe(df)
