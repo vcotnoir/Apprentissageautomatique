b1cbb603581bbd661b2893a795aef70e494a0f33
Vincent Cotnoir <cotnoir.vincent@gmail.com>
2023-11-25 18:02:56 -0500

    Update streamlit-apprentissage_automatique.py

--- streamlit-apprentissage_automatique.py
+++ streamlit-apprentissage_automatique.py
@@ -50,14 +50,35 @@ st.markdown(Explanation_text_traitement_donnes)
 st.header('Entrainement des modèles')
 
 Process_explnation_mrkdwn='''Voulant obtenir les meilleures performances, nous avons utilisé fait les étapes suivantes:  
-    1. Une cible fut établie, en utilisant la régle naive et en prédisant que tous les combats seraient gagnés par les combatants dans le coins rouge
-    2. Un MLP fut entrainé avec les réglages de base  
-    3. Un MLP fut entrainé en utilisant la recherche en grille  
-    4. Un MLP fut entrainé en utilisant la recherche aléatoire  
-    5. Un arbre de décision utilisant le boosting (XGBoost) fut entrainé  
-    6. Une regression linéaire utilisant l'analyse en compsantes principale fut entrainé  
+1. Une cible fut établie, en utilisant la régle naive et en prédisant que tous les combats seraient gagnés par les combatants dans le coins rouge
+2. Un MLP fut entrainé avec les réglages de base  
+3. Un MLP fut entrainé en utilisant la recherche en grille  
+4. Un MLP fut entrainé en utilisant la recherche aléatoire  
+5. Un arbre de décision utilisant le boosting (XGBoost) fut entrainé  
+6. Une regression linéaire utilisant l'analyse en compsantes principale fut entrainé  
 
 Nous avons également fait des essaits en modifiant notre jeux de données, mais aucun différence notable de fut notée et donc les résultats ne seront pas présentés. '''
 
 st.markdown(Process_explnation_mrkdwn)
 
+st.text('le code utilisé pour monter les modèles et les résultats sont présentés ci-bas:')
+
+with st.container():
+    tab3,tab4,tab5,tab6,tab7,tab8 = st.tabs(['Règle Naive', 'MLP','Gridsearch','Randomsearch','XGBoost','Regression linéaire'])
+
+    with tab3:
+        st.subheader("Données brutes")
+        st.dataframe(df)
+        st.write("Les données brutes ont ",shape_df_lignes," lignes et ",shape_df_colonnes," colonnes.")
+    with tab4:
+        st.subheader("Données traitées")
+        st.dataframe(df_preprocessed)
+        st.write("Les données traitées ont ",shape_df_processed_lignes," lignes et ",shape_df_processed_colonnes," colonnes.")
+    with tab5:
+        st.subheader("Données brutes")
+    with tab6:
+        st.subheader("Données brutes")
+    with tab7:
+        st.subheader("Données brutes")
+    with tab8:
+        st.subheader("Données brutes")
