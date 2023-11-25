4fd97bc6af2e2f6c7fa0af6017f9421e8c8b9c5e
Vincent Cotnoir <cotnoir.vincent@gmail.com>
2023-11-25 17:58:14 -0500

    Update streamlit-apprentissage_automatique.py

--- streamlit-apprentissage_automatique.py
+++ streamlit-apprentissage_automatique.py
@@ -44,18 +44,18 @@ Explanation_text_traitement_donnes='''Les principales modifications sont les sui
 - 
 - 
 '''
+
 st.markdown(Explanation_text_traitement_donnes)
 
 st.header('Entrainement des modèles')
-st.divider()
 
-Process_explnation_mrkdwn='''Voulant obtenir les meilleures performances, nous avons utilisé les modèles suivants.:  
-1. Une cible fut établie, en utilisant la régle naive et en prédisant que tous les combats seraient gagnés par les combatants dans le coins rouge
-2. Un MLP fut entrainé avec les réglages de base  
-3. Un MLP fut entrainé en utilisant la recherche en grille  
-4. Un MLP fut entrainé en utilisant la recherche aléatoire  
-5. Un arbre de décision utilisant le boosting (XGBoost) fut entrainé  
-6. Une regression linéaire utilisant l'analyse en compsantes principale fut entrainé  
+Process_explnation_mrkdwn='''Voulant obtenir les meilleures performances, nous avons utilisé fait les étapes suivantes:  
+    1. Une cible fut établie, en utilisant la régle naive et en prédisant que tous les combats seraient gagnés par les combatants dans le coins rouge
+    2. Un MLP fut entrainé avec les réglages de base  
+    3. Un MLP fut entrainé en utilisant la recherche en grille  
+    4. Un MLP fut entrainé en utilisant la recherche aléatoire  
+    5. Un arbre de décision utilisant le boosting (XGBoost) fut entrainé  
+    6. Une regression linéaire utilisant l'analyse en compsantes principale fut entrainé  
 
 Nous avons également fait des essaits en modifiant notre jeux de données, mais aucun différence notable de fut notée et donc les résultats ne seront pas présentés. '''
 
