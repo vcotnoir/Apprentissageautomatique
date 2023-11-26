4ed695b11be1372999f516d5213e27bb7e044de6
Vincent Cotnoir <cotnoir.vincent@gmail.com>
2023-11-26 01:15:42 -0500

    Update streamlit-apprentissage_automatique.py

--- streamlit-apprentissage_automatique.py
+++ streamlit-apprentissage_automatique.py
@@ -64,7 +64,7 @@ st.header('Analyse exploratoire')
 st.write('La corrélation entre les variables a été obtneu pour comprendre les données et savoir si certaines variables devraient êtres priorisées')
 
 correlation=sns.heatmap(df_correlation.corr(),cmap='coolwarm')
-correlation.set_title('Corrélation entre les variables');
+correlation.set_title('Corrélation entre les variables')
 st.pyplot(correlation.get_figure())
 
 st.write("Le paquet de visualisation Sweetviz a également été utilisé pour faire une analyse exploratoire des données")
