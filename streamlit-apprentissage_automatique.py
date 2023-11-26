a1a27eb09fc8dcf12307f1c69d7f237c1a1de65f
Vincent Cotnoir <cotnoir.vincent@gmail.com>
2023-11-25 23:10:51 -0500

    Update streamlit-apprentissage_automatique.py

--- streamlit-apprentissage_automatique.py
+++ streamlit-apprentissage_automatique.py
@@ -1,5 +1,4 @@
-import streamlit as st
-#import matplotlib as plt  
+import streamlit as st 
 import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score, f1_score, recall_score,confusion_matrix,ConfusionMatrixDisplay, precision_score
